c2901d8644af8ce8797abd52ca1e3e16bef1d4d5
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2024-09-24 12:59:21 -0700

    Time: 72 ms (12.93%), Space: 41.8 MB (100%) - LeetHub

--- 0873-guess-the-word/0873-guess-the-word.js
+++ 0873-guess-the-word/0873-guess-the-word.js
@@ -15,6 +15,28 @@
  * @param {Master} master
  * @return {void}
  */
-var findSecretWord = function(words, master) {
+const findSecretWord = (words, master) => {
+    let guess = words[randomizer(words.length)];
+    let matches = master.guess(guess);
     
-};
+    while(matches!== 6) {
+        for (let i = 0; i <= words.length - 1; i++) {
+            if (matchChecker(words[i], guess) !== matches) {
+                words.splice(i, 1)
+            }
+      } guess = words[randomizer(words.length)];
+        matches = master.guess(guess);
+    }
+  };
+
+  const randomizer = (arrL) => {
+      return Math.floor(Math.random() * arrL);
+  }
+
+const matchChecker = (word, guess) => {
+        let matches = 0;
+        for(let i = 0; i < 6; i++) {
+            word[i] === guess[i] ? matches++ : -1;
+    }
+    return matches;
+  }
