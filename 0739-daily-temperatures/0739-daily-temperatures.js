527b49003a7c7733ea4a9e2cde45685bb5785e6a
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2024-09-24 12:58:02 -0700

    Time: 4071 ms (5%), Space: 68.5 MB (99.6%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,7 +1,21 @@
 /**
- * @param {number[]} temperatures
+ * @param {number[]} temps
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+ //loop through the temp array
+ 
+var dailyTemperatures = function(temps) {
+    const n = temps.length;
+    const ans = new Array(n).fill(0);
+    console.log(ans);
+
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j < n; j++) {
+            if(temps[j] > temps[i]) {
+                ans[i] = j - i;
+                break;
+            } 
+        }
+    }
+    return ans;
 };
