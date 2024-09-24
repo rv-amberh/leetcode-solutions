f60f8b116f2b136fb784a89722e43e9a2988431b
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2024-09-24 12:54:55 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ //find the middle point and if our num is smaller than target we can start our search between there and the end of our array or the other way
+
+var search = function(nums, target) {
+    let midway = Math.round(nums.length/2)
+    console.log(midway)
+    if(nums[midway] < target) {
+        let lp = midway;
+        let rp = nums.length - 1;
+        while(lp <= rp) {
+            if(nums[lp] == target) return lp;
+            else if(nums[rp] == target) return rp;
+            else {
+                lp++;
+                rp--;
+            }
+        }
+    }
+    return -1
+};
