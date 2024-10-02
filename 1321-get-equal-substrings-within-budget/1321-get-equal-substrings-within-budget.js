f344e9382c5340554552cb9815fcecfe5e58ab56
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2024-10-02 11:23:22 -0700

    Time: 391 ms (5.63%), Space: 52.8 MB (16.9%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.js
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.js
@@ -4,6 +4,29 @@
  * @param {number} maxCost
  * @return {number}
  */
-var equalSubstring = function(s, t, maxCost) {
+const equalSubstring = (s, t, maxCost) => {
+    let ans = 0;
+    let costC = [];
     
+    for(let i = 0; i < s.length; i++) {
+        let cost = Math.abs(s.charCodeAt(i) - t.charCodeAt(i));
+        costC.push(cost);
+    }
+
+       let left = 0;
+        let curr = 0;
+
+    for(let j = 0; j < costC.length; j++) {
+
+        curr += costC[j];
+        console.log(curr, "curr")
+
+        while(curr > maxCost && left <= j) {
+            curr -= costC[left];
+            left+=1;
+        }
+        ans = Math.max(ans, j - left + 1);
+    }
+
+    return ans;
 };
