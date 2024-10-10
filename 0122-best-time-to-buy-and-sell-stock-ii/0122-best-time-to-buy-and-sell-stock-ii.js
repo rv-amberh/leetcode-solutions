86e4190b982652a089e4745358f435545f0b8f1f
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2024-10-10 16:32:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,27 +3,5 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-let profit = 0;
-let ans = 0;
-let lp = 0;
-let rp = 1;
-
-while(lp < rp && rp < prices.length) {
-    if(prices[lp] <= prices[rp]) {
-        if(prices[rp] - prices[lp] > profit) {
-             ans+= prices[rp] - prices[lp];
-             profit = 0;
-            lp = rp;
-            rp++;
-        } else {
-            console.log('profit', profit, prices[lp], prices[rp])
-            rp++;
-        }
-    } else if (prices[lp] > prices[rp]) {
-        ans += profit;
-        lp = rp;
-        rp++;
-    }
-  }
-  return ans;
+    
 };
